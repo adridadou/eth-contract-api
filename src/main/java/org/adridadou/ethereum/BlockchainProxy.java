44aad3864ff6457d873b7d3b03e05ef211021773
Kiril Ivanov <k.ivanov@secondfloor.nl>
2016-10-25 16:35:00 +0200

    Dropped JSONRpc as it's not needed anymore

--- src/main/java/org/adridadou/ethereum/BlockchainProxy.java
+++ src/main/java/org/adridadou/ethereum/BlockchainProxy.java
@@ -4,7 +4,6 @@ import org.adridadou.ethereum.handler.EthereumEventHandler;
 import org.adridadou.ethereum.smartcontract.SmartContract;
 import org.ethereum.core.TransactionReceipt;
 import org.ethereum.crypto.ECKey;
-import org.ethereum.jsonrpc.JsonRpc;
 import rx.Observable;
 
 /**
@@ -23,8 +22,6 @@ public interface BlockchainProxy {
     EthereumEventHandler events();
 
     boolean addressExists(EthAddress address);
-
-    JsonRpc getJsonRpc();
 }
 
 
