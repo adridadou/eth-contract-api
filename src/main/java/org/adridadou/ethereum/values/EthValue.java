93ea1fff48317da347d72292bfaf98a52fec3eda
adridadou <david@roon.me>
2016-11-13 14:01:08 +0100

    add equals + hashCode to comparable class

--- src/main/java/org/adridadou/ethereum/values/EthValue.java
+++ src/main/java/org/adridadou/ethereum/values/EthValue.java
@@ -35,4 +35,25 @@ public class EthValue implements Comparable<EthValue> {
     public int compareTo(EthValue o) {
         return value.compareTo(o.value);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        EthValue ethValue = (EthValue) o;
+
+        return value != null ? value.equals(ethValue.value) : ethValue.value == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return value != null ? value.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return value + " Wei";
+    }
 }
