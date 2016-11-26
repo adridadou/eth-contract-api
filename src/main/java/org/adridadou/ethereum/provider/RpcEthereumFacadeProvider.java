a308ce40caf5edecc75b7ba988763d8b46a6be25
adridadou <david@roon.me>
2016-11-26 17:23:44 +0100

    fix chain ID

--- src/main/java/org/adridadou/ethereum/provider/RpcEthereumFacadeProvider.java
+++ src/main/java/org/adridadou/ethereum/provider/RpcEthereumFacadeProvider.java
@@ -13,7 +13,7 @@ import org.web3j.protocol.http.HttpService;
 public class RpcEthereumFacadeProvider {
 
     public final static byte MAIN_CHAIN_ID = 0;
-    public final static byte ROPSTEN_CHAIN_ID = 2;
+    public final static byte ROPSTEN_CHAIN_ID = 3;
 
     public EthereumFacade create(final String url, final Byte chainId) {
         return create(new Web3JFacade(Web3j.build(new HttpService(url))), chainId);
