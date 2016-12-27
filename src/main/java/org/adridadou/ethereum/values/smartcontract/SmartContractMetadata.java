6c373954a7787091b1201f3f4d1210cd70caa32c
adridadou <david@roon.me>
2016-12-27 16:41:40 +0100

    change config

--- src/main/java/org/adridadou/ethereum/values/smartcontract/SmartContractMetadata.java
+++ src/main/java/org/adridadou/ethereum/values/smartcontract/SmartContractMetadata.java
@@ -16,4 +16,26 @@ public class SmartContractMetadata {
     public ContractAbi getAbi() {
         return abi;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SmartContractMetadata that = (SmartContractMetadata) o;
+
+        return abi != null ? abi.equals(that.abi) : that.abi == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return abi != null ? abi.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "SmartContractMetadata{" +
+                "abi=" + abi +
+                '}';
+    }
 }
